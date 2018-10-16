e5e3fc20862f07fbe3f4105968879406f5585bf4
minhaz <minhaztuhin@gmail.com>
2018-10-16 12:00:31 +0600

    Take all value by a function passing a parameter

--- firstJScalcu15_10_18.js
+++ firstJScalcu15_10_18.js
@@ -1,59 +1,14 @@
 var preValue = 0;
-var nowValue;
 var lastFinal=0;
 var totalValue;
 
 
-function zero(){
-    nowValue = document.getElementById("zero").value;
-	preValue = (preValue*10)+Number(nowValue);
+function sendValue(getValue){
+	preValue = (preValue*10)+Number(getValue);
     document.getElementById("display").value = preValue;
 }
-function one(){
-    nowValue = document.getElementById("one").value;
-	preValue = (preValue*10)+Number(nowValue);
-    document.getElementById("display").value = preValue;
-}
-function two(){
-    nowValue = document.getElementById("two").value;
-	preValue = (preValue*10)+Number(nowValue);
-    document.getElementById("display").value = preValue;
-}
-function three(){
-    nowValue = document.getElementById("three").value;
-	preValue = (preValue*10)+Number(nowValue);
-    document.getElementById("display").value = preValue;
-}
-function four(){
-    nowValue = document.getElementById("four").value;
-	preValue = (preValue*10)+Number(nowValue);
-    document.getElementById("display").value = preValue;
-}
-function five(){
-    nowValue = document.getElementById("five").value;
-	preValue = (preValue*10)+Number(nowValue);
-    document.getElementById("display").value = preValue;
-}
-function six(){
-    nowValue = document.getElementById("six").value;
-	preValue = (preValue*10)+Number(nowValue);
-    document.getElementById("display").value = preValue;
-}
-function seven(){
-    nowValue = document.getElementById("seven").value;
-	preValue = (preValue*10)+Number(nowValue);
-    document.getElementById("display").value = preValue;
-}
-function eight(){
-    nowValue = document.getElementById("eight").value;
-	preValue = (preValue*10)+Number(nowValue);
-    document.getElementById("display").value = preValue;
-}
-function nine(){
-    nowValue = document.getElementById("nine").value;
-	preValue = (preValue*10)+Number(nowValue);
-    document.getElementById("display").value = preValue;
-}
+
+
 function plus(){
     nowValue = document.getElementById("plus").value;
 	preValue = nowValue;
@@ -74,11 +29,6 @@ function div(){
 	preValue = nowValue;
     document.getElementById("display").value = preValue;
 }
-function dot(){
-    nowValue = document.getElementById("dot").value;
-	preValue = nowValue;
-    document.getElementById("display").value = preValue;
-}
 function result(){
 	totalValue = lastFinal+preValue;
 	document.getElementById("display").value = totalValue;
@@ -86,8 +36,7 @@ function result(){
 }
 function clar(){
 	preValue = 0;
-	nowValue = 0;
 	lastFinal= 0;
 	totalValue=0;
-	document.getElementById("display").value = lastFinal;
+	document.getElementById("display").value = null;
 }
